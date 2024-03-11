1094eb4518ebb7262980e19bf40482526b074531
VICTOR-M8 <darkhink8@gmail.com>
2024-03-11 19:07:36 +0100

    the cards were added

--- src/Components/CardProduct/CardProduct.tsx
+++ src/Components/CardProduct/CardProduct.tsx
@@ -1,9 +1,30 @@
 
+import './CardProduct.css';
 
-const CardProduct = () => {
-    return (
-        <div>CardProduct</div>
-    )
+type ProductType = {
+    name: string;
+    description: string;
+    price: number;
+    image: string;
 }
 
-export default CardProduct
+const CardProduct = ({ product }: { product: ProductType }) => {
+    const { name, description, price, image } = product;
+
+    return (
+        <main className="cardProductContainer">
+            <section>
+                <div className="card-product">
+                    <div className="card-image-container">
+                        <img src={image} alt={name} className="image-icon" />
+                    </div>
+                    <p className="card-title">{name}</p>
+                    <p className="card-des">{description}</p>
+                    <p className="card-price">${price}</p>
+                </div>
+            </section>
+        </main>
+    );
+}
+
+export default CardProduct;
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,13 +1,33 @@
 import "../Css/Home.css";
 import Slider from "../Components/Slider/Slider";
+import CardProduct from "../Components/CardProduct/CardProduct";
+import jsonData from "../service/product.json"
 
 
 const Home = () => {
     return (
-        <>
+        <main className="HomeContainer">
             <Slider />
-            
-        </>
+
+            <div className="HomeCardProductContainer">
+                <div className="text_container">
+                    <h2>Productos destacados</h2>
+                    <h4>que te encantaran</h4>
+                </div>
+
+                <div className="card_container">
+                    <CardProduct product={jsonData} />
+                    <CardProduct product={jsonData} />
+                    <CardProduct product={jsonData} />
+                    <CardProduct product={jsonData} />
+                    <CardProduct product={jsonData} />
+                    <CardProduct product={jsonData} />
+                </div>
+            </div>
+
+
+
+        </main>
     )
 }
 
