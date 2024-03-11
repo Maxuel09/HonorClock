e25920876b1928a70c3b767b3741f4d6a1557ad0
Davvidar <daargente@gmail.com>
2024-03-11 14:48:39 +0100

    list edit

--- src/Components/OrderList/OrderList.tsx
+++ src/Components/OrderList/OrderList.tsx
@@ -1,29 +1,21 @@
-
-import Badge from 'react-bootstrap/Badge';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Form from 'react-bootstrap/Form';
 
+
 const OrderList = () => {
-  return (
-    <ListGroup as="ol" numbered className="mt-1  ">
-    <ListGroup.Item
-        as="li"
-        className="d-flex  align-items-center"
-    >
-        <div className="ms-2 me-auto">
-            <div className="fw-bold">Reloj
-            </div>
-           
-        </div>
-        
-        <div className="me-5">eliminar</div>
-        <Badge bg="primary" pill>
-        <Form.Control placeholder="Stock" type="number" className="w-25" />
-        </Badge>
-        <div className="ms-5">eliminar</div>
-    </ListGroup.Item>
-</ListGroup>
-  )
+    return (
+        <ListGroup variant="flush">
+            <ListGroup.Item className='border'>
+                <div className='d-flex justify-content-between m-4'>
+                    <div>Producto</div>
+                    <div className=''> <Form.Control className='w-25' placeholder="unidades" type="number" /></div>
+                    <div>Eliminar</div>
+                    <div>2€</div>
+                </div>
+            </ListGroup.Item>
+
+        </ListGroup>
+    )
 }
 
 export default OrderList
--- src/Pages/Order.tsx
+++ src/Pages/Order.tsx
@@ -1,17 +1,35 @@
 
 import OrderList from '../Components/OrderList/OrderList';
-
+import Button from 'react-bootstrap/Button';
 const Order = () => {
     return (
-       <div className="mt-5">
-           
-          <OrderList />
-          <OrderList />
+        <div className="mt-5 d-flex flex-column">
+            <div className='bg-light'>
+                <h1 className='mt-4'>Carrito</h1>
+                <hr />
+            </div>
+            <div className="m-0">
+                <OrderList />
+                <OrderList />
+                
+            </div>
+            
+            <div className='bg-light d-flex justify-content-between'>
+                <div className='m-4'><h1 className='mb-2'>Total</h1>
+                
+                </div>
+                <div>
+                <h1 className='m-4 text-center'>23€</h1>
 
-       </div>
+                <Button className='m-4 ps-5 pe-5' variant="warning">Pagar</Button>
+                </div>
+                
+            </div>
 
-           
-       
+        </div>
+
+
+
     )
 }
 
