1fce96b6f128f9894ae0dd0ac8ef8d22b02e54a3
Aliciared7 <aliciared7@gmail.com>
2024-03-14 15:31:41 +0100

    add products to categories pages

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Navbar, Nav, Dropdown, Button, ButtonGroup} from 'react-bootstrap';
+import { Navbar, Nav} from 'react-bootstrap';
 
 const CustomNavbar = () => {
     return (
