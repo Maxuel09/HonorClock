f954a71897b945ed89d4d3f39f695e944a35b0f9
Aliciared7 <aliciared7@gmail.com>
2024-03-14 08:47:09 +0100

    add Mis productos

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -15,6 +15,9 @@ const Navbar = () => {
                     <Link className="nav-link text-white" to="/HC/Relojes">Relojes</Link>
                 </li>
                 <li className="nav-item">
+                    <Link className="nav-link text-white" to="/HC/Mis UserProducts">Mis Productos</Link>
+                </li>
+                <li className="nav-item">
                     <Link className="nav-link text-white" to="/HC/SignIn">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
                             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
